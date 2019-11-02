50835c01eabb8d338d82331f86f025bdaab4f2ca
Nattharee Pisarnsathukit <penquin501@gmail.com>
2019-11-02 10:59:58 +0700

    add service for search data by zipcode

--- src/Controller/ServiceController.php
+++ src/Controller/ServiceController.php
@@ -22,8 +22,33 @@ class ServiceController extends AbstractController
             "JOIN postinfo_province p ON d.PROVINCE_ID=p.PROVINCE_ID " .
             "JOIN postinfo_zipcodes z ON d.DISTRICT_CODE=z.district_code " .
             "WHERE d.DISTRICT_NAME = '".$keyword."' OR a.AMPHUR_NAME = '".$keyword."'";
-        $listDistrict = $entityManager->getConnection()->query($sql);
-        $addressInfo = json_decode($this->json($listDistrict)->getContent(), true);
+        $listAddress = $entityManager->getConnection()->query($sql);
+        $addressInfo = json_decode($this->json($listAddress)->getContent(), true);
+
+        if($addressInfo == null){
+            $output=['status'=>'ERROR_NOT_FOUND'];
+        } else {
+            $output=['status'=>'SUCCESS','addressInfo'=>$addressInfo];
+        }
+        return $this->json($output);
+    }
+
+    /**
+     * @Route("/service/search/data/zipcode/api", methods={"GET"})
+     */
+    public function searchDataZipcode(Request $request)
+    {
+        $zipcode = $request->query->get('zipcode');
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $sql = "SELECT d.DISTRICT_NAME,a.AMPHUR_NAME, p.PROVINCE_NAME,z.zipcode,z.DHL_SLA " .
+            "FROM postinfo_district d " .
+            "JOIN postinfo_amphur a ON d.AMPHUR_ID=a.AMPHUR_ID " .
+            "JOIN postinfo_province p ON d.PROVINCE_ID=p.PROVINCE_ID " .
+            "JOIN postinfo_zipcodes z ON d.DISTRICT_CODE=z.district_code " .
+            "WHERE z.zipcode = '".$zipcode."'";
+        $listAddress = $entityManager->getConnection()->query($sql);
+        $addressInfo = json_decode($this->json($listAddress)->getContent(), true);
 
         if($addressInfo == null){
             $output=['status'=>'ERROR_NOT_FOUND'];
